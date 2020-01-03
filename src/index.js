aaa42f2891880d3acbff294016386bd98f89f48c
AaronJThompson <aaronjthompson98@gmail.com>
2020-01-03 18:56:39 +0000

    Add mongoose connection

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
+
+let db = mongoose.connection;
+
+db.on('error', () => {
+  console.log(`---Mongoose failed to connect to url ${process.env.DB_URL}`);
+});
+
+db.once('open', () => {
+  console.log('+++Mongoose connected');
+})
