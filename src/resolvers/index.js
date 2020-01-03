e01c641c354319fa0c52d8059e42a3e64e838d3c
AaronJThompson <aaronjthompson98@gmail.com>
2020-01-03 19:59:55 +0000

    Add resolvers

---
+++ src/resolvers/index.js
@@ -0,0 +1,16 @@
+const { getBattlelog, getPlayer } = require('../data-sources/brawlstars');
+
+module.exports = {
+  Query: {
+    async player(parent, { tag }, context, info) {
+      const player = await getPlayer(args.tag);
+      return player;
+    }
+  },
+  Player: {
+    async battlelog(player, { tag }) {
+      const battlelog = await getBattlelog(tag || player.tag);
+      return battlelog;
+    }
+  }
+}
