34be058a0a3f6a907bc0b2944d1686ab1af95586
AaronJThompson <aaronjthompson98@gmail.com>
2020-01-03 19:09:15 +0000

    Add player graphql schema

---
+++ src/schemas/playerSchema.js
@@ -0,0 +1,14 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+  type Player {
+    tag: String!
+    name: String!
+    trophies: Int
+    battlelog: BattleLog
+  }
+
+  extend type Query {
+    player(tag: String!): Player
+  }
+`
